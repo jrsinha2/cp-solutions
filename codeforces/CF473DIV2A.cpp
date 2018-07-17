5e37a57f0dcc69c17ec7c474af0dc9382fffbaaa
Aditya Sinha <31968928+jrsinha2@users.noreply.github.com>
2018-07-17 20:18:32 +0530

    Create CF473DIV2A.cpp

---
+++ codeforces/CF473DIV2A.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<string>
+#include<cmath>
+#include<cstdio>
+#include<cctype>
+#include<algorithm>
+#include<climits>
+#include<queue>
+#include<list>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+#define f(i,a,n) for(ll i=a;i<n;i++)
+#define fr(i,a,n)	for(i=n-1;i>=a;i--)
+#define MAX 100001
+#define PI	3.141516
+
+int main()
+{	ll n;
+	cin>>n;
+	if(n%2==0)
+		cout<<"Mahmoud\n";
+	else
+		cout<<"Ehab\n";
+	return 0;
+}
